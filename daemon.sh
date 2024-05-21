dea77fc9153f33bb6a7ac58a61343db6df548be2
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-05-21 13:36:10 +0200

    Experiment management changed

--- daemon.sh
+++ daemon.sh
@@ -10,11 +10,11 @@
 
 framework_dir=`dirname $0`
 export CODE_PATH=$(readlink -f $framework_dir )
-CONFIG_DAEMON=$CODE_PATH'/config_daemon'
-export module=$1 # For setting global vars from config_daemon according to the stage
+CONFIG_DAEMON=$1
+#CONFIG_DAEMON=$CODE_PATH'/config_daemon'
+export module=$2 # For setting global vars from config_daemon according to the stage
 source $CONFIG_DAEMON
 export PATH=$LAB_SCRIPTS:$PATH
-export PATH=$CODE_PATH'/reports:'$PATH
 export PATH=$CODE_PATH'/scripts:'$PATH
 export PATH=$CODE_PATH'/aux_sh:'$PATH
 export TEMPLATES=$CODE_PATH'/templates'
@@ -24,15 +24,16 @@ export TEMPLATES=$CODE_PATH'/templates'
 ## STAGE EXECUTION
 #######################################################################
 
-if [ "$module" == "0" ] ; then
-    # STAGE 0 CONVERTING BCL FILES INTO FASTQ
-    echo "Launching stage 0: Converting BCL files into FASTQ"
-    if [ $launch_login == TRUE ]; then  
-        cellranger_mkfastq.sh
-    else
-        sbatch aux_sh/cellranger_mkfastq.sh
-    fi
-elif [ "$module" == "1" ] ; then
+# if [ "$module" == "0" ] ; then
+#     # STAGE 0 CONVERTING BCL FILES INTO FASTQ
+#     echo "Launching stage 0: Converting BCL files into FASTQ"
+#     if [ $launch_login == TRUE ]; then  
+#         cellranger_mkfastq.sh
+#     else
+#         sbatch aux_sh/cellranger_mkfastq.sh
+#     fi
+# el
+if [ "$module" == "1" ] ; then
     mkdir -p $FULL_RESULTS
     while IFS= read sample; do
         echo $sample
@@ -50,14 +51,4 @@ elif [ "$module" == "2" ] ; then
     else
         sbatch aux_sh/compare_samples.sh
     fi
-
-elif [ "$module" == "3" ] ; then
-    # STAGE 3 GENERAL PREPROCESSING REPORT
-    echo "Launching stage 3: General preprocessing report"
-    if [ $launch_login == TRUE ]; then  
-        general_report.sh
-    else
-        sbatch aux_sh/general_report.sh
-    fi
-
 fi
