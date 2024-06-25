2efd9b59bf03a153b210b0a33bf7700395274a39
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-06-25 12:27:23 +0200

    Compare_samples.sh: Removed manual version control lines. Provided more resources to script. Removed conditional for integrative analysis, as it will always be performed.

--- aux_sh/compare_samples.sh
+++ aux_sh/compare_samples.sh
@@ -1,13 +1,11 @@
 #! /usr/bin/env bash
 
-# Sergio Alias, 20230530
-# Last modified 20230721
 
 # STAGE 2 SAMPLES COMPARISON
 
 #SBATCH -J compare_samples.sh
-#SBATCH --cpus-per-task=12
-#SBATCH --mem='200gb'
+#SBATCH --cpus-per-task=48
+#SBATCH --mem='1000gb'
 #SBATCH --constraint=cal
 #SBATCH --time=0-23:00:00
 #SBATCH --error=job.comp.%J.err
@@ -20,7 +18,6 @@
 hostname
 
 mkdir -p $report_folder
-
 
 cat $FULL_RESULTS/*/metrics > $experiment_folder'/metrics'
 cat $FULL_RESULTS/*/cellranger_metrics > $experiment_folder'/cellranger_metrics'
@@ -51,25 +48,30 @@ create_metric_table.rb $experiment_folder'/cellranger_metrics' sample $experimen
                                --resolution $preproc_resolution \
                                --int_sec_cond $int_sec_cond
 
-if [ "$integrative_analysis" == "TRUE" ] ; then
-    mkdir -p $FULL_RESULTS/$subset_column
-    integration.R --output $FULL_RESULTS/$subset_column \
-                  --name $subset_column \
-                  --filter $preproc_filter \
-                  --mincells $preproc_init_min_cells \
-                  --minfeats $preproc_init_min_feats \
-                  --minqcfeats $preproc_qc_min_feats \
-                  --percentmt $preproc_max_percent_mt \
-                  --normalmethod $preproc_norm_method \
-                  --scalefactor $preproc_scale_factor \
-                  --hvgs $preproc_select_hvgs \
-                  --ndims $preproc_pca_n_dims \
-                  --dimheatmapcells $preproc_pca_n_cells \
-                  --report_folder $report_folder \
-                  --experiment_name $experiment_name \
-                  --resolution $preproc_resolution \
-                  --exp_design $exp_design \
-                  --condition $subset_column \
-                  --count_path $FULL_RESULTS"/*/cellranger_0000/*" \
-                  --suffix "outs/filtered_feature_bc_matrix"
-fi
+
+mkdir -p $FULL_RESULTS/$experiment_name
+integration.R --output $FULL_RESULTS/$experiment_name \
+              --project_name $experiment_name \
+              --filter $preproc_filter \
+              --mincells $preproc_init_min_cells \
+              --minfeats $preproc_init_min_feats \
+              --minqcfeats $preproc_qc_min_feats \
+              --percentmt $preproc_max_percent_mt \
+              --normalmethod $preproc_norm_method \
+              --scalefactor $preproc_scale_factor \
+              --hvgs $preproc_select_hvgs \
+              --ndims $preproc_pca_n_dims \
+              --dimheatmapcells $preproc_pca_n_cells \
+              --report_folder $report_folder \
+              --experiment_name $experiment_name \
+              --resolution $preproc_resolution \
+              --exp_design $exp_design \
+              --int_columns $int_columns \
+              --count_path $FULL_RESULTS"/*/cellranger_0000/*" \
+              --suffix "outs/filtered_feature_bc_matrix" \
+              --samples_to_integrate "$samples_to_integrate" \
+              --annotation_dir $annotation_dir \
+              --markers_general $markers_dir/markers-general \
+              --markers_specific $markers_dir/markers-specific \
+              --markers_canonical $markers_dir/markers-canonical \
+              --cpu $SLURM_CPUS_PER_TASK
