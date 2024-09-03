46c9211eff89845cbac774a653d4637f63704056
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-09-03 16:52:17 +0200

    Updated get_clusters_distribution and get_query_distribution tests

--- tests/testthat/test-get_clusters_distribution.R
+++ tests/testthat/test-get_clusters_distribution.R
@@ -7,9 +7,9 @@ test_that("get_clusters_distribution properly calculates percentages", {
   expected_pct[1, ] <- c(0.6, 0.4, 0)
   expected_pct[2, ] <- c(0, 1.0, 0)
   expected_pct[3, ] <- c(0, 0.6, 0.4)
-  expected_pct <- expected_pct * 100
+  expected_pct <- data.frame(expected_pct) * 100
+  colnames(expected_pct) <- 1:3
+  rownames(expected_pct) <- c("A", "B", "C")
   output_pct <- get_clusters_distribution(pbmc_smaller, 2)
-  dimnames(output_pct) <- NULL
-  output_pct <- apply(output_pct, 2, rbind)
   expect_equal(output_pct, expected_pct)
 })
