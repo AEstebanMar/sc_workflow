eadb52e0fdbcd032b967b18cd34611e9b25201e0
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-09-11 16:27:40 +0200

    test-annotate_clusters.R: changed test, as annotate_clusters no longer checks for multiple cell types being assigned to a certain cluster

--- tests/testthat/test-annotate_clusters.R
+++ tests/testthat/test-annotate_clusters.R
@@ -1,4 +1,4 @@
-test_that("annotate_clusters properly marks duplicated cell types", {
+test_that("annotate_clusters simply assigns names to clusters", {
   library(Seurat)
   pbmc_smaller <- pbmc_small[, 1:15]
   Idents(pbmc_smaller) <- 1:15
@@ -7,8 +7,8 @@ test_that("annotate_clusters properly marks duplicated cell types", {
   new_clusters[c(2, 5, 13)] <- "TypeC"
   annotated <- suppressWarnings(annotate_clusters(pbmc_smaller, new_clusters))
   output <- annotated@meta.data$named_clusters
-  expected <- c("TypeA (a)", "TypeC (a)", "TypeB (a)", "TypeA (b)", "TypeC (b)",
-                "TypeA (c)", "TypeB (b)", "TypeA (d)", "TypeA (e)", "TypeB (c)",
-                "TypeA (f)", "TypeA (g)", "TypeC (c)", "TypeA (h)", "TypeA (i)")
+  expected <- c("TypeA", "TypeC", "TypeB", "TypeA", "TypeC",
+                "TypeA", "TypeB", "TypeA", "TypeA", "TypeB",
+                "TypeA", "TypeA", "TypeC", "TypeA", "TypeA")
   expect_equal(as.character(output), expected)
 })
