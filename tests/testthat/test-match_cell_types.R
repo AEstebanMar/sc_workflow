b0491b45ff8918337acaeb7432911f51c01544a4
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-09-13 11:42:19 +0200

    test-match_cell_types.R: added test for draws between multiple cell types in cluster assignation. Fixed tests, were defining expected genes on the assumption that they would be found in row names

--- tests/testthat/test-match_cell_types.R
+++ tests/testthat/test-match_cell_types.R
@@ -8,7 +8,6 @@ test_that("Cell type matching sorted simple case", {
 							 type = c("Type1", "Type2", "Type3"))
 	expected_df <- markers_df
 	expected_df$cell_type <- c("1. Type1", "2. Type2", "3. Type3", "4. Unknown")
-	expected_df$gene <- rownames(expected_df)
 	expected_df <- expected_df[order(expected_df$cluster), ]
 	output_df <- match_cell_types(markers_df = markers_df,
 								  cell_annotation = cell_annotation)$stats_table
@@ -25,7 +24,6 @@ test_that("Cell type matching unsorted simple case", {
 							 type = c("Type3", "Type2", "Type1"))
 	expected_df <- markers_df
 	expected_df$cell_type <- c("2. Type1", "3. Type3", "1. Type2", "4. Unknown")
-	expected_df$gene <- rownames(expected_df)
 	expected_df <- expected_df[order(expected_df$cluster), ]
 	output_df <- match_cell_types(markers_df, cell_annotation)$stats_table
 	expect_equal(output_df, expected_df)
@@ -43,7 +41,6 @@ test_that("Cell type matching complex case", {
 	expected_df <- markers_df
 	expected_df$cell_type <-  c(rep("1. type1", 20), rep("2. type2", 20),
 								rep("3. type3", 10))
-	expected_df$gene <- rownames(expected_df)
 	expected_df <- expected_df[order(expected_df$cluster), ]
 	output_df <- match_cell_types(markers_df, cell_annotation)$stats_table
 	expect_equal(output_df, expected_df)
@@ -61,6 +58,22 @@ test_that("More than one cluster assigned the same cell type", {
 	expected_df <- markers_df
 	expected_df$cell_type <- c("1. type1 (a)", "2. type2", "3. type3",
 							   "4. type1 (b)")
+	output_df <- match_cell_types(markers_df, cell_annotation)$stats_table
+	expect_equal(output_df, expected_df)
+})
+
+test_that("Draw between more than one cell type", {
+	markers_df <- data.frame(samples = 1:4, cluster = 1:4)
+	markers_df$p_val_adj <- rep(1e-5, 4)
+	markers_df$avg_log2FC <- rep(1, 4)
+	genes <- paste0("gene", c(1, 1, 2))
+	genes <- c(genes, paste0("gene", 1))
+	markers_df$gene <- genes
+	types <- c("type1", "type2", "type3")
+	cell_annotation <- data.frame(markers = genes[1:12], type = types)
+	expected_df <- markers_df
+	expected_df$cell_type <- c("1. type1 / type2 (a)", "2. type1 / type2 (b)",
+							   "3. type3", "4. type1 / type2 (c)")
 	output_df <- match_cell_types(markers_df, cell_annotation)$stats_table
 	expect_equal(output_df, expected_df)
 })
