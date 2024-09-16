042fbd0ffd6b1fae07f542ff094a0afd8541478b
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-09-16 16:29:07 +0200

    test_breakdown_query.R: Optimised test structure. Removed alternate "by" arguments test, as that behaviour is no longer included in breakdown_query. test-get_query_pct.R: updated tests, more closely resemble seurat clusters. Optimised test structure. Added test for get_query_pct with alternate "by" arguments.

--- tests/testthat/test-breakdown_query.R
+++ tests/testthat/test-breakdown_query.R
@@ -1,57 +1,31 @@
 
+library(Seurat)
+pbmc_smaller <- pbmc_small[, 1:15]
+pbmc_smaller@meta.data$sample <- c(rep("A", 5), rep("B", 5), rep("C", 5))
+pbmc_smaller@meta.data$seurat_clusters <- c(rep(0, 5), rep (1, 5), rep (2, 5))
+query <- c("MS4A1", "CD79A", "HLA-DRB5")
 test_that("breakdown_query works in simple case", {
-  library(Seurat)
-  pbmc_smaller <- pbmc_small[, 1:15]
-  pbmc_smaller@meta.data$sample <- c(rep("A", 5), rep("B", 5), rep("C", 5))
-  query <- c("MS4A1", "CD79A", "HLA-DRB5")
-  expected_df <- matrix(nrow = 3, ncol = 3)
-  rownames(expected_df) <- c("A", "B", "C")
-  expected_df[1, ] <- rep(0, 3)
-  expected_df[2, ] <- c(0, 0, 20)
-  expected_df[3, ] <- c(100, 80, 80)
-  colnames(expected_df) <- query
-  output_df <- breakdown_query(pbmc_smaller, query, "sample")
+  expected_df <- c(0.33, 0.27, 0.33)
+  names(expected_df) <- query
+  output_df <- signif(breakdown_query(pbmc_smaller, query), 2)
   expect_equal(output_df, expected_df)
 })
 
 test_that("breakdown_query gives warning if any query genes are not found", {
   library(Seurat)
-  pbmc_smaller <- pbmc_small[, 1:15]
-  pbmc_smaller@meta.data$sample <- c(rep("A", 5), rep("B", 5), rep("C", 5))
-  query <- c("MS4A1", "CD79A", "HLA-DRB5", "NOEXPA", "NOEXPB")
-  expected_df <- matrix(nrow = 3, ncol = 3)
-  rownames(expected_df) <- c("A", "B", "C")
-  expected_df[1, ] <- rep(0, 3)
-  expected_df[2, ] <- c(0, 0, 20)
-  expected_df[3, ] <- c(100, 80, 80)
-  colnames(expected_df) <- query[1:3]
-  expect_warning(breakdown_query(pbmc_smaller, query, "sample"), "NOEXPA, NOEXPB")
-  output_df <- suppressWarnings(breakdown_query(pbmc_smaller, query, "sample"))
+  missing_query <- c(query, "NOEXPA", "NOEXPB")
+  expected_df <- c(0.33, 0.27, 0.33)
+  names(expected_df) <- query
+  expect_warning(breakdown_query(pbmc_smaller, missing_query), "NOEXPA, NOEXPB")
+  output_df <- suppressWarnings(signif(breakdown_query(pbmc_smaller,
+                                                       missing_query), 2))
   expect_equal(output_df, expected_df)
 })
 
 test_that("breakdown_query works with query of length one", {
-  library(Seurat)
-  pbmc_smaller <- pbmc_small[, 1:15]
-  pbmc_smaller@meta.data$sample <- c(rep("A", 5), rep("B", 5), rep("C", 5))
-  query <- c("HLA-DRB5")
-  expected_df <- matrix(nrow = 3, ncol = 1)
-  rownames(expected_df) <- c("A", "B", "C")
-  expected_df[, 1] <- c(0, 20, 80)
-  colnames(expected_df) <- query
-  output_df <- breakdown_query(pbmc_smaller, query, "sample")
+  single_query <- c("HLA-DRB5")
+  expected_df <- 0.33
+  names(expected_df) <- single_query
+  output_df <- signif(breakdown_query(pbmc_smaller, single_query), 2)
   expect_equal(output_df, expected_df)
-})
-
-test_that("breakdown_query works with alternate 'by' arguments", {
-  library(Seurat)
-  pbmc_smaller <- pbmc_small[, 1:15]
-  pbmc_smaller@meta.data$seurat_clusters <- c(rep(0, 5), rep (1, 5), rep (2, 5))
-  query <- c("HLA-DRB5")
-  expected_df <- matrix(nrow = 3, ncol = 1)
-  rownames(expected_df) <- c(0:2)
-  expected_df[, 1] <- c(0, 20, 80)
-  colnames(expected_df) <- query
-  output_df <- breakdown_query(pbmc_smaller, query, "seurat_clusters")
-  testthat::expect_equal(output_df, expected_df)
 })
--- tests/testthat/test-get_query_pct.R
+++ tests/testthat/test-get_query_pct.R
@@ -1,9 +1,10 @@
 library(Seurat)
 pbmc_smaller <- pbmc_small[, 1:15]
 pbmc_smaller@meta.data$sample <- c(rep("A", 5), rep("B", 5), rep("C", 5))
-pbmc_smaller@meta.data$seurat_clusters <- rep(1:5, 3)
+pbmc_smaller@meta.data$seurat_clusters <- rep(0:4, 3)
 cell_types <- c("typeA", "typeB", "typeC", "typeD", "typeE")
 pbmc_smaller@meta.data$named_clusters <- rep(cell_types, 3)
+query <- c("MS4A1", "CD79A", "HLA-DRB5")
 test_that("get_query_pct works in simple case", {
   query <- c("MS4A1", "CD79A", "HLA-DRB5")
   expected_df <- matrix(nrow = 3, ncol = 3)
@@ -17,30 +18,39 @@ test_that("get_query_pct works in simple case", {
 })
 
 test_that("get_query_pct gives warning if any query genes are not found", {
-  query <- c("MS4A1", "CD79A", "HLA-DRB5", "NOEXPA", "NOEXPB")
+  missing_query <- c("MS4A1", "CD79A", "HLA-DRB5", "NOEXPA", "NOEXPB")
   expected_df <- matrix(nrow = 3, ncol = 3)
   rownames(expected_df) <- c("A", "B", "C")
   expected_df[1, ] <- rep(0, 3)
   expected_df[2, ] <- c(0, 0, 20)
   expected_df[3, ] <- c(100, 80, 80)
-  colnames(expected_df) <- query[1:3]
-  expect_warning(get_query_pct(pbmc_smaller, query, "sample"), "NOEXPA, NOEXPB")
-  output_df <- suppressWarnings(get_query_pct(pbmc_smaller, query, "sample"))
+  colnames(expected_df) <- query
+  expect_warning(get_query_pct(pbmc_smaller, missing_query, "sample"), "NOEXPA, NOEXPB")
+  output_df <- suppressWarnings(get_query_pct(pbmc_smaller, missing_query, "sample"))
   expect_equal(output_df, expected_df)
 })
 
 test_that("get_query_pct works with query of length one", {
-  query <- c("HLA-DRB5")
+  single_query <- c("HLA-DRB5")
   expected_df <- matrix(nrow = 3, ncol = 1)
   rownames(expected_df) <- c("A", "B", "C")
   expected_df[, 1] <- c(0, 20, 80)
-  colnames(expected_df) <- query
-  output_df <- get_query_pct(pbmc_smaller, query, "sample")
+  colnames(expected_df) <- single_query
+  output_df <- get_query_pct(pbmc_smaller, single_query, "sample")
   expect_equal(output_df, expected_df)
 })
 
+test_that("get_query_pct works with alternate 'by' arguments", {
+  single_query <- c("HLA-DRB5")
+  expected_df <- matrix(nrow = 5, ncol = 1)
+  rownames(expected_df) <- c(0:4)
+  expected_df[, 1] <- c(33, 67, 33, 0, 33)
+  colnames(expected_df) <- single_query
+  output_df <- get_query_pct(pbmc_smaller, single_query, "seurat_clusters")
+  testthat::expect_equal(output_df, expected_df)
+})
+
 test_that("get_query_pct works with 'by' argument of length 2", {
-  query <- c("MS4A1", "CD79A", "HLA-DRB5")
   types <- paste0("type", toupper(letters[1:5]))
   matA <- matrix(data = 0, nrow = 5, ncol = 3)
   matB = matC <- matA
