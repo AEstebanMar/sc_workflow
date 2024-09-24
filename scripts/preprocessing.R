46110b78af91ca4818ecdb0bea2a27bf89781f86
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-09-24 13:33:53 +0200

    preprocessing.R: Added query to parser and to main_analyze_seurat function call. Added block to process target genes before calling

--- scripts/preprocessing.R
+++ scripts/preprocessing.R
@@ -59,7 +59,9 @@ option_list <- list(
   optparse::make_option(c("--resolution"), type = "double",
               help="Granularity of the clustering"),
   optparse::make_option(c("--exp_design"), type = "character",
-              help="Path to experiment design file")
+              help="Path to experiment design file"),
+  optparse::make_option("--target_genes", type = "character", default = "",
+              help = "Path to target genes table, or comma-separated list of target genes")
 )  
 
 
@@ -79,6 +81,15 @@ input <- file.path(opt$input, ifelse(opt$filter, "filtered_feature_bc_matrix",
 
 exp_design <- read.table(opt$exp_design, sep = "\t", header = TRUE)
 
+if(opt$target_genes == ""){
+  warning("No target genes provided")
+  target_genes <- NULL
+} else if(file.exists(opt$target_genes)) {
+  target_genes <- read_and_format_targets(opt$target_genes)
+} else {
+  target_genes <- list(Custom = strsplit(opt$target_genes, split = ";")[[1]])
+}
+
 seu <- read_input(name = opt$name, input = input, mincells = opt$mincells,
                   minfeats = opt$minfeats, exp_design = exp_design)
 
@@ -86,7 +97,7 @@ all_seu <- main_analyze_seurat(seu = seu, output = opt$report_folder, hvgs = opt
                                minqcfeats = opt$minqcfeats, ndims = opt$ndims,
                                percentmt = opt$percentmt,
                                resolution = opt$resolution,
-                               integrate = FALSE)
+                               integrate = FALSE, query = unlist(target_genes))
 
 write_sergio_report(all_seu = all_seu, minqcfeats = opt$minqcfeats,
                     template = file.path(template_path,
