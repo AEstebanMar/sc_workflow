ac144325bfcaa9088fb580c8b59a4736c30fa226
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-11-18 10:25:34 +0100

    test-collapse_markers.R: updated test to new column of output data frame

--- tests/testthat/test-collapse_markers.R
+++ tests/testthat/test-collapse_markers.R
@@ -3,11 +3,11 @@ test_that("collapse_markers base use case", {
   markers_1 <- data.frame(a_avg_log2FC = rep(1, 3), b_avg_log2FC = rep (-1, 3))
   markers_2 <- data.frame(a_avg_log2FC = rep(-2, 3), b_avg_log2FC = rep (2, 3))
   markers_list <- list(cluster_1 = markers_1, cluster_2 = markers_2)
-  expected_df <-
   output_df <- collapse_markers(markers_list)
   expected_df <- data.frame(gene = as.character(rep(1:3, 2)),
                             a_avg_log2FC = c(rep(1, 3), rep(-2, 3)),
                             b_avg_log2FC = c(rep(-1, 3), rep (2, 3)),
-                            cluster = c(rep(0, 3), rep(1, 3)))
+                            cluster = c(rep(0, 3), rep(1, 3)),
+                            avg_log2FC = rep(0, 6))
   expect_equal(output_df, expected_df)
 })
