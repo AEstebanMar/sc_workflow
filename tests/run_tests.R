0126367823c8811d767925088a23dcc5d18e7153
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-11-27 12:11:09 +0100

    run_test.R: created new file to run entire test suite

---
+++ tests/run_tests.R
@@ -0,0 +1,10 @@
+#! /usr/bin/env Rscript
+# Custom test file for non-package structure
+
+
+library(testthat)
+library(Seurat)
+source("../scripts/lib/sc_library.R")
+source("../scripts/lib/main_analyze_seurat.R")
+
+test_dir("./testthat/")
