aaf30b22d723f2c73adbdf9b823b735dc1bb2bb8
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-04-01 16:22:32 +0200

    Added annotate_sc.sh script. Placeholder for now

--- aux_sh/annotate_sc.sh
+++ aux_sh/annotate_sc.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 
-# STAGE 2 SAMPLES COMPARISON
+# STAGE 3 EXPERIMENT ANNOTATION
 
 #SBATCH -J annotate_sc.sh
 #SBATCH --cpus-per-task=36
@@ -17,53 +17,54 @@ if [ -z $SLURM_CPUS_PER_TASK ]; then
     SLURM_CPUS_PER_TASK=1
 fi
 
-. ~soft_bio_267/initializes/init_ruby
 . ~aestebanm/initializes/init_Hunter_dev
+
 hostname
 mkdir -p $output"/report"
 
 exp_doublet_file=""
 
 annotate_sc.R --output $output \
-            --name $experiment_name \
-            --doublet_file "$exp_doublet_file" \
-            --filter $preproc_filter \
-            --mincells $preproc_init_min_cells \
-            --minfeats $preproc_init_min_feats \
-            --minqcfeats $preproc_qc_min_feats \
-            --percentmt $preproc_max_percent_mt \
-            --normalmethod $preproc_norm_method \
-            --scalefactor $preproc_scale_factor \
-            --hvgs $preproc_select_hvgs \
-            --ndims $preproc_pca_n_dims \
-            --dimheatmapcells $preproc_pca_n_cells \
-            --resolution $preproc_resolution \
-            --exp_design $exp_design \
-            --subset_by "$subset_by" \
-            --input $FULL_RESULTS"/*/cellranger_0000/*" \
-            --suffix "outs/filtered_feature_bc_matrix" \
-            --cluster_annotation "$cluster_annotation" \
-            --target_genes $target_genes \
-            --cpu $SLURM_CPUS_PER_TASK \
-            --imported_counts "$imported_counts" \
-            --cell_annotation "$cell_annotation" \
-            --SingleR_ref "$refs_path/$SingleR_ref" \
-            --ref_version "$ref_version" \
-            --ref_label "$ref_label" \
-            --ref_de_method "$ref_de_method" \
-            --ref_n "$ref_n" \
-            --p_adj_cutoff $p_adj_cutoff \
-            --verbose $verbose \
-            --reduce $reduce \
-            --samples_to_integrate $samples_to_process \
-            --integrate TRUE \
-            --int_method "$int_method" \
-            --saveRDS $saveRDS \
-            --loadRDS $loadRDS \
-            --filter_dataset "$filter_dataset" \
-            --sketch $sketch \
-            --sketch_pct $sketch_pct \
-            --sketch_method "$sketch_method"  \
-            --force_ncells "$force_ncells" \
-            --extra_columns "$extra_columns" \
-            --k_weight $k_weight #& process_monitoring.sh R $output/exec_params
+              --name $experiment_name \
+              --doublet_file "$exp_doublet_file" \
+              --filter $preproc_filter \
+              --mincells $preproc_init_min_cells \
+              --minfeats $preproc_init_min_feats \
+              --minqcfeats $preproc_qc_min_feats \
+              --percentmt $preproc_max_percent_mt \
+              --normalmethod $preproc_norm_method \
+              --scalefactor $preproc_scale_factor \
+              --hvgs $preproc_select_hvgs \
+              --ndims $preproc_pca_n_dims \
+              --dimheatmapcells $preproc_pca_n_cells \
+              --resolution $preproc_resolution \
+              --exp_design $exp_design \
+              --subset_by "$subset_by" \
+              --input $FULL_RESULTS"/*/cellranger_0000/*" \
+              --suffix "outs/filtered_feature_bc_matrix" \
+              --cluster_annotation "$cluster_annotation" \
+              --target_genes $target_genes \
+              --cpu $SLURM_CPUS_PER_TASK \
+              --imported_counts "$imported_counts" \
+              --cell_annotation "$cell_annotation" \
+              --SingleR_ref "$refs_path/$SingleR_ref" \
+              --ref_version "$ref_version" \
+              --ref_label "$ref_label" \
+              --ref_de_method "$ref_de_method" \
+              --ref_n "$ref_n" \
+              --p_adj_cutoff $p_adj_cutoff \
+              --verbose $verbose \
+              --reduce $reduce \
+              --samples_to_integrate $samples_to_process \
+              --integrate TRUE \
+              --int_method "$int_method" \
+              --saveRDS $saveRDS \
+              --loadRDS $loadRDS \
+              --filter_dataset "$filter_dataset" \
+              --sketch $sketch \
+              --sketch_pct $sketch_pct \
+              --sketch_method "$sketch_method"  \
+              --force_ncells "$force_ncells" \
+              --extra_columns "$extra_columns" \
+              --k_weight $k_weight \
+              --genome $genome #& process_monitoring.sh R $output/exec_params
