af55cdaad71144cc0b69acdcc3f05015530e1130
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-04-01 16:23:05 +0200

    Progressed developing sc_Hunter.sh script. Returns results, now only missing report.

--- aux_sh/sc_Hunter.sh
+++ aux_sh/sc_Hunter.sh
@@ -1,5 +1,32 @@
 #! /usr/bin/env bash
 
 
-ajaj
+# STAGE 4 DEG ANALYSIS
 
+#SBATCH -J sc_Hunter.sh
+#SBATCH --cpus-per-task=12
+#SBATCH --mem='300gb'
+#SBATCH --constraint=cal
+#SBATCH --time=0-5:00:00
+#SBATCH --error=job.DEG.%J.err
+#SBATCH --output=job.DEG.%J.out
+
+if [ -z $SLURM_CPUS_PER_TASK ]; then
+    SLURM_CPUS_PER_TASK=1
+fi
+
+. ~aestebanm/initializes/init_Hunter_dev
+hostname
+mkdir -p $output"/report"
+
+sc_Hunter.R --DEG_target $DEG_target \
+            --input $output/counts \
+            --target_genes $target_genes \
+            --p_val_cutoff $DEG_p_val_cutoff \
+            --min_avg_log2FC $min_avg_log2FC \
+            --min_cell_proportion $min_cell_proportion \
+            --min_counts $min_counts \
+            --output $output \
+            --name $experiment_name \
+            --verbose $verbose \
+            --cpu $SLURM_CPUS_PER_TASK
