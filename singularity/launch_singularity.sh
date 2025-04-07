af1302b3414a74c2bb6713810c377ce7089b5649
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-04-07 16:22:54 +0200

    Changed name of log files

--- singularity/launch_singularity.sh
+++ singularity/launch_singularity.sh
@@ -5,8 +5,8 @@
 #SBATCH --mem='700gb'
 #SBATCH --constraint=cal
 #SBATCH --time=0-20:00:00
-#SBATCH --error=job.seu.%J.err
-#SBATCH --output=job.seu.%J.out
+#SBATCH --error=job.sing.%J.err
+#SBATCH --output=job.sing.%J.out
 
 
 if [ "$1" == "" ]; then
