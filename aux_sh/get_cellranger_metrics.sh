7e29e927ad6a231be9309c01dafe1022e74867e4
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-05-28 16:24:32 +0200

    Added "count" and "multi" mode to metrics script

--- aux_sh/get_cellranger_metrics.sh
+++ aux_sh/get_cellranger_metrics.sh
@@ -4,6 +4,7 @@
 input=$1
 sample=$2
 output=$3
+mode=$4
 
 if [ "$input" == "" ]; then
 	echo No input specified
@@ -20,13 +21,27 @@ if [ "$output" == "" ]; then
 	exit 1
 fi
 
-head -n 1 $input | tr "," "\n" > tmp.header
-tail -n 1 $input | sed 's/\%,/\n/g' | sed 's/\",/\n/g' | tr -d "\"" > tmp.content
-paste tmp.header tmp.content -d "\t" > tmp
-
 rm $output
-while IFS= read line; do
-	echo -e "$sample\t$line" >> $output
-done < tmp
 
-rm tmp.header tmp.content tmp
+if [ "$mode" == "count" ]; then
+	head -n 1 $input | tr "," "\n" > tmp.header
+	tail -n 1 $input | sed 's/\%,/\n/g' | sed 's/\",/\n/g' | tr -d "\"" > tmp.content
+	paste tmp.header tmp.content -d "\t" > tmp
+
+	while IFS= read line; do
+		echo -e "$sample\t$line" >> $output
+	done < tmp
+	rm tmp.header tmp.content
+
+elif [ "$mode" == "multi" ]; then
+	cut -f 5- $input -d "," | grep -v "Sample ID" | grep -v "per probe barcode" | tr -d "\"" > tmp
+	while IFS= read line; do
+		new_line=`echo $line | sed 's/,/\t/1' | tr -d ","`
+		echo -e $sample"\t""$new_line" >> $output
+	done < tmp
+else
+	echo "Unrecognized mode value. Must be \"count\" or \"multi\", was \"$mode\""
+	exit 1
+fi
+
+rm tmp
