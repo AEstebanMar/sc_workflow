09662016bfc4b49d431038e69ac1a7f74b4b4af2
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-09-22 15:56:25 +0200

    Updated arguments passed to R script, and changed initialiser to Hunter devel init

--- aux_sh/get_SingleR_ref.sh
+++ aux_sh/get_SingleR_ref.sh
@@ -10,6 +10,7 @@
 #SBATCH --time=1-23:59:59
 #SBATCH --error=job.ref.%J.err
 #SBATCH --output=job.ref.%J.out
+
 aux_opt=$1
 if [ -z $SLURM_CPUS_PER_TASK ]; then
     CPU=1
@@ -17,9 +18,7 @@ else
     CPU=$SLURM_CPUS_PER_TASK
 fi
 
-source ~soft_bio_267/initializes/init_R
-
-get_SingleR_ref.R --reference "$SingleR_ref" --version "$ref_version" \
-                  --verbose $verbose --output "$ref_origin" \
-                  --quiet $quiet --database "$ref_to_process" \
-                  --ref_label "$ref_label" --cpu $CPU $aux_opt
+source ~aestebanm/initializes/init_Hunter_dev
+get_SingleR_ref.R --reference "$ref_to_process" --name "$ref_name" --cpu $CPU \
+                  --version "$ref_version" --verbose $verbose --quiet $quiet \
+                  --output "$ref_output_path" --ref_label "$label_to_trim" $aux_opt
