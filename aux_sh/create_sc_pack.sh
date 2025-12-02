46b4dd1bae92c7ca3f34861df9949fbb7c49ba33
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-12-02 12:28:03 +0100

    Updated pack preparation script, no longer includes rds files, and adds functional enrichment reports

--- aux_sh/create_sc_pack.sh
+++ aux_sh/create_sc_pack.sh
@@ -4,9 +4,15 @@ output_folder=$CODE_PATH/results/$experiment_name
 
 rm -rf $output_folder
 mkdir -p $output_folder
-mkdir -p $output_folder
+
+DEG_reports=`find $output/report/ -name *DEG_report.html`
 
 cp $output/report/$experiment_name"_All_samples_QC_report.html" $output_folder
-cp $output/report/$experiment_name"_integration_report.html" $output_folder
+cp $output/report/$experiment_name"_annotation_report.html" $output_folder
 cp $output/report/$experiment_name"_qc_report.html" $output_folder
-cp $output/$experiment_name".final_results.rds" $output_folder
+cp $DEG_reports $output_folder
+
+for enr_folder in `ls $output/report | grep clust_enr`; do
+	mkdir -p $output_folder"/"$enr_folder
+	cp -r $output"/report/"$enr_folder/*html $output_folder"/$enr_folder"
+done
